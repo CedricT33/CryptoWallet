76e642c57c9b423b4dd0db23546f03847bac53b0
CedricT33 <cedric.tressous@gmail.com>
2021-02-22 19:57:50 +0100

    initialisation

---
+++ src/script.js
@@ -0,0 +1,10 @@
+/** ----- VALUES ------ */
+
+
+/** -----AU CHARGEMENT DU DOM----- */
+function onDocumentReady() {
+    
+}
+
+/** -----AU CHARGEMENT DU DOM----- */
+document.onload = onDocumentReady();
