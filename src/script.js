4ca93e6eae0b0223801e925dac790a0bba6b53ff
CedricT33 <cedric.tressous@gmail.com>
2021-02-25 20:33:06 +0100

    Ecran de présentation

--- src/script.js
+++ src/script.js
@@ -1,9 +1,29 @@
 /** ----- VALUES ------ */
+var version = '';
+var storage = [];
 
+/**
+ * AFFICHE LA PRESENTATION DE L'APPLI SI LE LOCALSTORAGE EST VIDE
+ */
+function gestionAffichagePresentation() {
+    var elmtFleche = document.getElementById('fleche-presentation');
+    var elmtPresentation = document.getElementById('presentation-container');
 
-/** -----AU CHARGEMENT DU DOM----- */
+    if (!storage || storage.length == 0) {
+        elmtFleche.classList.remove('hide');
+        elmtPresentation.classList.remove('hide');
+    }
+    else {
+        elmtFleche.classList.add('hide');
+        elmtPresentation.classList.add('hide');
+    }
+}
+
+/**
+ * LANCE LES FONCTIONS DE DEMARRAGE DE L'APP
+ */
 function onDocumentReady() {
-    
+    gestionAffichagePresentation();
 }
 
 /** -----AU CHARGEMENT DU DOM----- */
